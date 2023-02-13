7b5388edd8784afdccc7a75a01b136d3cec56979
Sarthak Jain <77235755+sarthak17jain@users.noreply.github.com>
2023-02-13 16:52:19 +0530

    update1

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ app.use('/payment', paymentRouter);
 app.use('/cart', cartRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
+    app.use(express.static( 'public/build' ));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'public', 'build', 'index.html')); // relative path
