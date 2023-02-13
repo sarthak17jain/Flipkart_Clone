9c98e8987a1fc4b972f4db7a457d03176f5f93b5
Sarthak Jain <77235755+sarthak17jain@users.noreply.github.com>
2023-02-13 10:23:34 +0530

    update4

--- server/index.js
+++ server/index.js
@@ -58,7 +58,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'build', 'index.html')); // relative path
+        // res.sendFile(path.join(__dirname, 'public', 'build', 'index.html')); // relative path
+        res.sendFile('./public/build/index.html'); // relative path
     });
 }
 //PAYTM:
