d8deb0e096d5578f669ef770b1934ebe81106470
sarthak17jain <sarthak.jain.ug20@nsut.ac.in>
2024-06-08 07:43:20 +0530

    moved index.js inside api

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,91 @@
-const app = require('../index.js');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const path = require('path');
+// import path from 'path';
+// import { v4 as uuid } from 'uuid';
 
+const DefaultData = require('../default.js');
+// const productRouter = require('./routes/productRouter.js');
+// const cartRouter = require('./routes/cartRouter.js');
+// const authRouter = require('./routes/authRouter.js');
+// const paymentRouter = require('./routes/paymentRouter.js');
+const routes = require('../routes/Router.js');
+
+const app = express();
+console.log()
+const PORT = process.env.PORT || 8000;
+const db_link = process.env.DB_LINK;
+console.log(process.env.PORT);
+console.log(process.env.CLIENT_URL);
+
+//rawBody in form of buffer data is required for Stripe 
+//req.body -> parsed JSON
+//req.rawData -> req in form on buffer
+app.use(bodyParser.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf
+    }
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+//cors with options credentials: true and origin: .. is required for cookie to be sent and received 
+app.use(cors({credentials: true, origin: process.env.CLIENT_URL}));
+//we use middleware function for cookieParser so that we can access our cookies from anywhere
+app.use(cookieParser());
+
+async function startServer() {
+    await DefaultData();
+    app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+}
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(db_link)
+.then(function(db){
+    // console.log(db);
+    console.log('db connected');
+    startServer();
+})
+.catch(function(err){
+    console.log(err);
+});
+
+
+// app.use('/productData', productRouter);
+// app.use('/auth', authRouter);
+// app.use('/payment', paymentRouter);
+// app.use('/cart', cartRouter);
+app.use('/api', routes);
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( '../public/build' ));
+    console.log(__dirname);
+    console.log(path.resolve(__dirname, '../public/build', 'index.html'));
+    app.get('*', (req, res) => {
+        // res.sendFile(path.join(__dirname, 'public', 'build', 'index.html')); // relative path
+        res.sendFile(path.resolve(__dirname, '../public/build', 'index.html'));
+    });
+}
+
+//For serverless vercel
 module.exports = app;
+
+//PAYTM:
+// export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+// //callback url is called after success or failure of payment
+// export let paytmParams = {
+
+// };
+// paytmParams['MID'] = process.env.PAYTM_MID,
+// paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+// paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+// paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+// paytmParams['ORDER_ID'] = uuid(),
+// paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
+// paytmParams['CALLBACK_URL'] = 'http://localhost:8000/payment/callback'
+// paytmParams['MOBILE_NO'] = '1234567852'
